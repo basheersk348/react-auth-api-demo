1c7b27f3e2169d6ccadc3343e20557cde3af2bcc
Basheer <basheersk348@gmail.com>
2026-08-13 18:47:10 +0530

    Switch to HashRouter to fix GitHub Pages 404 on login navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import Home from './components/Home.jsx';
@@ -35,7 +35,6 @@ function MainRoutes() {
 
   return (
     <Routes>
-      {/* Root URL http://localhost:3000 renders Login directly */}
       <Route
         path="/"
         element={
@@ -44,7 +43,7 @@ function MainRoutes() {
           </PublicRoute>
         }
       />
-      
+
       <Route
         path="/signup"
         element={
@@ -70,8 +69,8 @@ function MainRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <MainRoutes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
